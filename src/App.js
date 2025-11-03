5ade262ea773f94e3f7d86c1d64b6d4b661f17c3
Lei <gabriel_sl19798@hotmail.com>
2025-11-03 18:49:23 +1300

    just change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'public/car_plate.jpg'} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is lei Song
         </p>
         <a
           className="App-link"
